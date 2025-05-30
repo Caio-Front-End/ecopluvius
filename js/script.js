fe8c5de08b38b0e29e52edcb722cb7a9e6bedd8a
Caio Nascimento Battista <nascimento_caio_@hotmail.com>
2025-05-30 06:26:10 -0300

    feat(header): adiciona script para abrir menu mobile com efeito

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//Recuperando elementos do navbar
+const toggleButton = document.getElementById('menu-toggle');
+const navMenu = document.querySelector('nav');
+const navItems = navMenu.querySelectorAll('li');
+
+//Adicionando evento ao click no menu hamburguer
+toggleButton.addEventListener('click', () => {
+    //Quando clicado, abre a aba lateral com as opções do navbar 
+    navMenu.classList.toggle('open');
+    //Pra cada item dentro do meu navbar(<li>), eu adiciono uma classe para criar efeito 
+    navItems.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.add('visible');
+        }, index * 100); // atraso em sequência de acordo com o índice
+        //Removo a classe imediatamente para tornar possível ao fechar e abrir novamente
+        item.classList.remove('visible')
+    });
+});
