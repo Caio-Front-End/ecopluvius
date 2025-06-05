880db069d1fd232a108dacc825c3b74db36a5887
Caio Nascimento Battista <nascimento_caio_@hotmail.com>
2025-06-05 09:46:30 -0300

    chore(script.js): Importação do script da seção publico-alvo para o arquivo principal

--- js/script.js
+++ js/script.js
@@ -6,8 +6,11 @@ import { problemaBehavior } from "./problema.js";
 import { tecnologiasBehavior } from "./tecnologias.js";         
 //Função que adiciona comportamento aos objetivos   
 import { objetivosBehavior } from "./objetivos.js";
+//Função que adiciona comportamento ao público alvo
+import { publicoBehavior } from "./publico.js";
 
 headerBehavior();
 problemaBehavior();
 tecnologiasBehavior();
 objetivosBehavior();
+publicoBehavior();
